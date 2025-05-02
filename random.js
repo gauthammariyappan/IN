b666258afcc16e9494fcf592dd2f958cce5f6595
gauthammariyappan <60974224+gauthammariyappan@users.noreply.github.com>
2025-05-02 21:45:50 +0530

    Update random.js

--- random.js
+++ random.js
@@ -10,8 +10,33 @@ function getRandomName() {
   "Lopez", "Clark", "White", "Rodriguez", "Lewis", "Hall",
   "Walker", "Young", "King", "Hill", "Scott", "Adams"];
   
-  const randomFirstName = firstName[Math.floor(Math.random() * firstName.length)];
-  const randomLastName = lastName[Math.floor(Math.random() * lastName.length)];
+  let randomName = sessionStorage.getItem('randomName');
   
-  return `${randomFirstName} ${randomLastName}`;
+  if (!randomName) {
+    const randomFirstName = firstName[Math.floor(Math.random() * firstName.length)];
+    const randomLastName = lastName[Math.floor(Math.random() * lastName.length)];
+    sessionStorage.setItem('randomName', randomFirstName+' '+randomLastName);
+    return `${randomFirstName} ${randomLastName}`;
+  }
+  return randomName;
+  
 }
+
+function getAppKey(){
+  let appKey = '0670f4f40e3fb44d4e3df59d6894e4c7';
+  return appKey;
+
+}
+
+(function(w,d,s,r,k,h,m){
+	if(w.performance && w.performance.timing && w.performance.navigation) {
+		w[r] = w[r] || function(){(w[r].q = w[r].q || []).push(arguments)};
+		h=d.createElement('script');h.async=true;h.setAttribute('src',s+k);
+		d.getElementsByTagName('head')[0].appendChild(h);
+		(m = window.onerror),(window.onerror = function (b, c, d, f, g) {
+		m && m(b, c, d, f, g),g || (g = new Error(b)),(w[r].q = w[r].q || []).push(["captureException",g]);})
+	}
+})(window,document,'//static.site24x7rum.com/beacon/site24x7rum-min.js?appKey=','s247r',getAppKey());
+s247r('setWaterfallsSamplingRate',1);
+s247r('userId',getRandomName());
+s247r('recordSession',true);
